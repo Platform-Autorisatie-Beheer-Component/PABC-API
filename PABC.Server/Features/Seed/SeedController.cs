817feb4e9c613ac9120e77e6c3e82a6712fa281a
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-06-11 17:52:57 +0200

    change seed with a bit more realistic data

--- PABC.Server/Features/Seed/SeedController.cs
+++ PABC.Server/Features/Seed/SeedController.cs
@@ -36,8 +36,13 @@ namespace PABC.Server.Features.Seed
               await db.FunctionalRoles.AnyAsync() ||
               await db.Mappings.AnyAsync()) return false;
 
-            var appRole1 = new ApplicationRole { Id = Guid.NewGuid(), Application = "MyApplication1", Name = "MyAppRole1" };
-            var appRole2 = new ApplicationRole { Id = Guid.NewGuid(), Application = "MyApplication2", Name = "MyAppRole2" };
+            var appRoleZac1 = new ApplicationRole { Id = Guid.NewGuid(), Application = "ZAC", Name = "Raadpleger" };
+            var appRoleZac2 = new ApplicationRole { Id = Guid.NewGuid(), Application = "ZAC", Name = "Behandelaar" };
+            var appRoleZac3 = new ApplicationRole { Id = Guid.NewGuid(), Application = "ZAC", Name = "Coördinator" };
+            var appRoleZac4 = new ApplicationRole { Id = Guid.NewGuid(), Application = "ZAC", Name = "Recordmanager" };
+            var appRoleZac5 = new ApplicationRole { Id = Guid.NewGuid(), Application = "ZAC", Name = "Beheerder" };
+
+            var appRoleApp2 = new ApplicationRole { Id = Guid.NewGuid(), Application = "MyApplication2", Name = "MyAppRole2" };
 
             var funcRole1 = new FunctionalRole { Id = Guid.NewGuid(), Name = "MyFuncRole1" };
             var funcRole2 = new FunctionalRole { Id = Guid.NewGuid(), Name = "MyFuncRole2" };
@@ -48,15 +53,18 @@ namespace PABC.Server.Features.Seed
             var entityType4 = new EntityType { Id = Guid.NewGuid(), EntityTypeId = Guid.NewGuid().ToString(), Type = "zaaktype", Name = "My Zaaktype 4" };
             var entityType5 = new EntityType { Id = Guid.NewGuid(), EntityTypeId = Guid.NewGuid().ToString(), Type = "zaaktype", Name = "My Zaaktype 5" };
 
-            var domain1 = new Domain { Id = Guid.NewGuid(), Description = "This is my first domain", Name = "MyDomain1", EntityTypes = [entityType1, entityType2, entityType3] };
-            var domain2 = new Domain { Id = Guid.NewGuid(), Description = "This is my second domain", Name = "MyDomain2", EntityTypes = [entityType3, entityType4, entityType5] };
+            var domain1 = new Domain { Id = Guid.NewGuid(), Description = "Domein bedrijfsvoering gaat over bedrijfsvoering", Name = "Bedrijfsvoering", EntityTypes = [entityType1, entityType2, entityType3] };
+            var domain2 = new Domain { Id = Guid.NewGuid(), Description = "Domein leefomgeving gaat over leefomgeving", Name = "Leefomgeving", EntityTypes = [entityType3, entityType4, entityType5] };
+            var domain3 = new Domain { Id = Guid.NewGuid(), Description = "Domein ruimte gaat over ruimte", Name = "Ruimte", EntityTypes = [entityType3, entityType4, entityType5] };
 
-            var mapping1 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRole1, Domain = domain1, FunctionalRole = funcRole1 };
-            var mapping2 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRole1, Domain = domain2, FunctionalRole = funcRole2 };
-            var mapping3 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRole2, Domain = domain1, FunctionalRole = funcRole2 };
-            var mapping4 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRole2, Domain = domain2, FunctionalRole = funcRole1 };
+            var mapping1 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRoleZac1, Domain = domain1, FunctionalRole = funcRole1 };
+            var mapping2 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRoleZac2, Domain = domain2, FunctionalRole = funcRole2 };
+            var mapping3 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRoleZac3, Domain = domain1, FunctionalRole = funcRole2 };
+            var mapping4 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRoleZac4, Domain = domain2, FunctionalRole = funcRole1 };
+            var mapping5 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRoleZac5, Domain = domain3, FunctionalRole = funcRole1 };
+            var mapping6 = new Mapping { Id = Guid.NewGuid(), ApplicationRole = appRoleApp2, Domain = domain3, FunctionalRole = funcRole1 };
 
-            await db.AddRangeAsync(mapping1, mapping2, mapping3, mapping4);
+            await db.AddRangeAsync(mapping1, mapping2, mapping3, mapping4, mapping5, mapping6);
             var result = await db.SaveChangesAsync();
             return result > 0;
         }
