f7f630ad5fd478f5fcef0f5c1a0bef05dfb493a9
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-06-12 17:31:15 +0200

    add dummy test to prove we can test with a live postgres db

---
+++ PABC.Server.Test/Features/GetApplicationRolesPerEntityType/GetApplicationRolesPerEntityTypeControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using PABC.Server.Features.GetApplicationRolesPerEntityType;
+using PABC.Server.Test.TestConfig;
+
+namespace PABC.Server.Test.Features.GetApplicationRolesPerEntityType
+{
+    public class GetApplicationRolesPerEntityTypeControllerTests(PostgresFixture postgresFixture) : IClassFixture<PostgresFixture>
+    {
+        [Fact]
+        public async Task DummyTest()
+        {
+            var controller = new GetApplicationRolesPerEntityTypeController(postgresFixture.DbContext);
+            var result = await controller.Post(new GetApplicationRolesRequest { FunctionalRoleNames = [] });
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ PABC.Server.Test/TestConfig/PostgresFixture.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PABC.Data;
+
+namespace PABC.Server.Test.TestConfig
+{
+    public sealed class PostgresFixture : IAsyncLifetime
+    {
+        private readonly DistributedApplication _app;
+        private readonly IResourceBuilder<PostgresServerResource> _postgres;
+        private IServiceScope? _serviceScope;
+
+        public PabcDbContext DbContext { get; private set; } = null!;
+
+        public PostgresFixture()
+        {
+            var options = new DistributedApplicationOptions { AssemblyName = typeof(PostgresFixture).Assembly.FullName, DisableDashboard = true };
+            var appBuilder = DistributedApplication.CreateBuilder(options);
+            _postgres = appBuilder.AddPostgres("postgres");
+            _postgres.AddDatabase("Pabc");
+            _app = appBuilder.Build();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _app.StartAsync();
+            await _app.ResourceNotifications.WaitForResourceHealthyAsync("Pabc");
+            var _postgresConnectionString = await _postgres.Resource.GetConnectionStringAsync();
+            var services = new ServiceCollection();
+            services.AddDbContext<PabcDbContext>(opt => opt.UseNpgsql(_postgresConnectionString));
+            _serviceScope = services.BuildServiceProvider().CreateScope();
+            DbContext = _serviceScope.ServiceProvider.GetRequiredService<PabcDbContext>();
+            await DbContext.Database.MigrateAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            _serviceScope?.Dispose();
+            await _app.StopAsync();
+            if (_app is IAsyncDisposable asyncDisposable)
+            {
+                await asyncDisposable.DisposeAsync().ConfigureAwait(false);
+            }
+            else
+            {
+                _app.Dispose();
+            }
+        }
+    }
+}
