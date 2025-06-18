9b8ee9c64686c20f2cdb741fdc94b4a3e5689b94
Felix Cornelissen <felix@icatt.nl>
2025-06-18 11:06:05 +0200

    fix: the api should return 400 when the request contains invalid roles

--- PABC.Server/Features/GetApplicationRolesPerEntityType/GetApplicationRolesPerEntityTypeController.cs
+++ PABC.Server/Features/GetApplicationRolesPerEntityType/GetApplicationRolesPerEntityTypeController.cs
@@ -17,9 +17,26 @@ public class GetApplicationRolesPerEntityTypeController(PabcDbContext db) : Cont
     [ProducesResponseType<GetApplicationRolesResponse>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)]
     [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.ProblemJson)]
     [ProducesResponseType<ProblemDetails>(StatusCodes.Status401Unauthorized, MediaTypeNames.Application.ProblemJson)]
-    public async Task<ActionResult<GetApplicationRolesResponse>> Post([FromBody] GetApplicationRolesRequest request)
+    public async Task<ActionResult<GetApplicationRolesResponse>> Post([FromBody] GetApplicationRolesRequest request, CancellationToken token = default)
     {
-        var query = db.Mappings
+        var matchingFunctionalRoleNames = await db.FunctionalRoles
+            .Where(x => request.FunctionalRoleNames.Contains(x.Name))
+            .Select(x => x.Name)
+            .ToListAsync(token);
+
+        var unknownRoles = request.FunctionalRoleNames.Except(matchingFunctionalRoleNames).ToList();
+
+        if (unknownRoles.Count > 0)
+        {
+            return BadRequest(new ValidationProblemDetails
+            {
+                Title = "Unknown functional roles",
+                Detail = $"The following functional roles are unknown: {string.Join(", ", unknownRoles)}",
+                Status = StatusCodes.Status400BadRequest
+            });
+		}
+
+		var query = db.Mappings
             .Where(x => request.FunctionalRoleNames.Contains(x.FunctionalRole.Name))
             .SelectMany(x => x.Domain.EntityTypes, (m, e) => new { ApplicationRoleName = m.ApplicationRole.Name, m.ApplicationRole.Application, e.Id, e.Type, e.EntityTypeId, EntityTypeName = e.Name });
 
