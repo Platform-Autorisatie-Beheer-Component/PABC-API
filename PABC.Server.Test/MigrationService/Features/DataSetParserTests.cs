528029e0c122485d02526c7f3f7e9873ebac2445
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-07-21 17:33:45 +0200

    add tests

---
+++ PABC.Server.Test/MigrationService/Features/DataSetParserTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using PABC.MigrationService.Features.DatabaseInitialization;
+
+namespace PABC.Server.Test.MigrationService.Features
+{
+    public class DataSetParserTests
+    {
+        [Fact]
+        public async Task Empty_object_fails_with_required_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError("""
+            {
+            }
+            """);
+            Assert.Equal("required", error.Key);
+        }
+
+        [Fact]
+        public async Task Missing_applicationRoles_fails_with_required_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError("""
+            {
+                "functionalRoles": [],
+                "domains": [],
+                "entityTypes": [],
+                "mappings": []
+            }
+            """);
+            Assert.Equal("required", error.Key);
+        }
+
+        [Fact]
+        public async Task Missing_functionalRoles_fails_with_required_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError("""
+            {
+                "applicationRoles": [],
+                "domains": [],
+                "entityTypes": [],
+                "mappings": []
+            }
+            """);
+            Assert.Equal("required", error.Key);
+        }
+
+        [Fact]
+        public async Task Missing_domains_fails_with_required_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError("""
+            {
+                "applicationRoles": [],
+                "functionalRoles": [],
+                "entityTypes": [],
+                "mappings": []
+            }
+            """);
+            Assert.Equal("required", error.Key);
+        }
+
+        [Fact]
+        public async Task Missing_entityTypes_fails_with_required_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError("""
+            {
+                "applicationRoles": [],
+                "functionalRoles": [],
+                "domains": [],
+                "mappings": []
+            }
+            """);
+            Assert.Equal("required", error.Key);
+        }
+
+        [Fact]
+        public async Task Missing_mappings_fails_with_required_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError("""
+            {
+                "applicationRoles": [],
+                "functionalRoles": [],
+                "domains": [],
+                "entityTypes": []
+            }
+            """);
+            Assert.Equal("required", error.Key);
+        }
+
+        [Fact]
+        public async Task All_root_level_properties_empty_array_succeeds()
+        {
+            await Test("""
+            {
+                "applicationRoles": [],
+                "functionalRoles": [],
+                "domains": [],
+                "entityTypes": [],
+                "mappings": []
+            } 
+            """);
+        }
+
+        [Fact]
+        public async Task Index_property_too_long_fails_with_maxLength_error()
+        {
+            var error = await TestThrowsJsonSchemaValidationExceptionWithSingleError($$"""
+            {
+                "applicationRoles": [],
+                "functionalRoles": [
+                    {
+                      "id": "{{Guid.NewGuid()}}",
+                      "name": "{{TextOf257Characters}}"
+                    }
+                ],
+                "domains": [],
+                "entityTypes": [],
+                "mappings": []
+            } 
+            """);
+            Assert.Equal("maxLength", error.Key);
+        }
+
+        private static async Task<DataSet> Test([StringSyntax("json")] string input)
+        {
+            var parser = new DatasetParser();
+            await using var stream = new MemoryStream();
+            stream.Write(Encoding.UTF8.GetBytes(input));
+            stream.Seek(0, SeekOrigin.Begin);
+            return await parser.Parse(stream, CancellationToken.None);
+        }
+
+        private static async Task<KeyValuePair<string, string>> TestThrowsJsonSchemaValidationExceptionWithSingleError([StringSyntax("json")] string input)
+        {
+            var exception = await Assert.ThrowsAsync<JsonSchemaValidationException>(() => Test(input));
+            var singleRootLevelError = Assert.Single(exception.Errors);
+            return Assert.Single(singleRootLevelError.Errors);
+        }
+
+        const string TextOf257Characters = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, s";
+    }
+}
---
+++ PABC.Server.Test/MigrationService/Features/DatabaseInitializerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PABC.Data.Entities;
+using PABC.MigrationService.Features.DatabaseInitialization;
+using PABC.Server.Test.TestConfig;
+
+namespace PABC.Server.Test.MigrationService.Features
+{
+    public class DatabaseInitializerTests(PostgresFixture fixture) : IClassFixture<PostgresFixture>, IAsyncLifetime
+    {
+        public async Task InitializeAsync()
+        {
+            await ClearDatabaseAsync();
+            await fixture.DbContext.AddRangeAsync([
+                new FunctionalRole{ Id = Guid.NewGuid(), Name = Guid.NewGuid().ToString() }
+            ]);
+            await fixture.DbContext.SaveChangesAsync();
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        private async Task ClearDatabaseAsync()
+        {
+            var dbContext = fixture.DbContext;
+            await dbContext.ApplicationRoles.ExecuteDeleteAsync();
+            await dbContext.FunctionalRoles.ExecuteDeleteAsync();
+            await dbContext.Domains.ExecuteDeleteAsync();
+            await dbContext.EntityTypes.ExecuteDeleteAsync();
+            await dbContext.Mappings.ExecuteDeleteAsync();
+        }
+
+        [Fact]
+        public async Task If_dataSet_is_null_no_data_is_deleted()
+        {
+            var initializer = new DatabaseInitializer(fixture.DbContext);
+
+            await initializer.Initialize(null, CancellationToken.None);
+
+            var functionalRoleCount = await fixture.DbContext.FunctionalRoles.CountAsync();
+            Assert.Equal(1, functionalRoleCount);
+        }
+
+        [Fact]
+        public async Task If_dataSet_is_empty_all_data_is_deleted()
+        {
+            var initializer = new DatabaseInitializer(fixture.DbContext);
+
+            await initializer.Initialize(new DataSet { ApplicationRoles = [], Domains = [], EntityTypes = [], FunctionalRoles = [], Mappings = [] }, CancellationToken.None);
+
+            var functionalRoleCount = await fixture.DbContext.FunctionalRoles.CountAsync();
+            Assert.Equal(0, functionalRoleCount);
+        }
+    }
+}
