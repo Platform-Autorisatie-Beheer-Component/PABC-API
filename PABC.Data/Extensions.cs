c2794e8ba9b208f9a30a63f6e5a100d17bbe8396
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-09-08 11:01:08 +0200

    apply workaround to fix migrations

--- PABC.Data/Extensions.cs
+++ PABC.Data/Extensions.cs
@@ -1,6 +1,8 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Hosting;
-using EFCore.NamingConventions;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Migrations.Internal;
 
 namespace PABC.Data;
 
@@ -12,10 +14,29 @@ public static class Extensions
             connectionName: connectionName,
             configureDbContextOptions: options =>
             {
+
                 options.UseNpgsql()
+                .ReplaceService<IHistoryRepository, PascalCaseHistoryContext>()
                 .UseSnakeCaseNamingConvention();
             });
 
         return builder;
+    }
+
+    /// <summary>
+    /// workaround to keep the migrations working after using the snake case naming convention
+    /// </summary>
+    /// <param name="dependencies"></param>
+#pragma warning disable EF1001 // Internal EF Core API usage.
+    private class PascalCaseHistoryContext(HistoryRepositoryDependencies dependencies) : NpgsqlHistoryRepository(dependencies)
+#pragma warning restore EF1001 // Internal EF Core API usage.
+    {
+        protected override void ConfigureTable(EntityTypeBuilder<HistoryRow> history)
+        {
+            base.ConfigureTable(history);
+
+            history.Property(h => h.MigrationId).HasColumnName("MigrationId");
+            history.Property(h => h.ProductVersion).HasColumnName("ProductVersion");
+        }
     }
 }
