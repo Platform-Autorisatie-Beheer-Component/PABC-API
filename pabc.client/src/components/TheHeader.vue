bea18b9ae2ac01272caceabbf497ba1beddfa543
nijmra <armijn@gmail.com>
2025-09-18 12:23:21 +0200

    front-end base

---
+++ pabc.client/src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <span class="heading-1">PABC</span>
+  </header>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pabc.client/src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import DashboardView from "@/views/DashboardView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "dashboard",
+      component: DashboardView,
+      meta: {
+        title: "PABC"
+      }
+    }
+  ]
+});
+
+const title = document.title;
+
+router.beforeEach((to) => {
+  document.title = `${to.meta?.title ? to.meta.title + " | " : ""}${title}`;
+});
+
+export default router;
