bbe2834d964cef4e3554c3860eb6535654e86661
nijmra <armijn@gmail.com>
2025-09-18 17:40:38 +0200

    setup controllers

---
+++ PABC.Server/Features/Domains/DomainUpsertModel.cs
@@ -0,0 +1,13 @@
+﻿using PABC.Data;
+
+namespace PABC.Server.Features.Domains
+{
+    public class DomainUpsertModel
+    {
+        [Json.Schema.Generation.MaxLength(PabcDbContext.MaxLengthForIndexProperties)]
+        public required string Name { get; init; }
+
+        [Json.Schema.Generation.MaxLength(PabcDbContext.MaxLengthForIndexProperties)]
+        public required string Description { get; init; }
+    }
+}
---
+++ PABC.Server/Features/Domains/PutDomainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PABC.Data;
+
+namespace PABC.Server.Features.Domains
+{
+    //[ApiController]
+    //[Route("/api/v1/domains")]
+    //public class PutDomainController(PabcDbContext db) : Controller
+    //{
+    //    [HttpPut("{id}")]
+    //}
+}
