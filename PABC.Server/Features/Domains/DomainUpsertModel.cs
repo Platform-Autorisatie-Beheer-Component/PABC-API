cf20e9e3ca350fa47d1d193f2af8a4d94e5c5bc9
nijmra <armijn@gmail.com>
2025-09-23 15:01:50 +0200

    maxlength inputs, client/server validation

--- PABC.Server/Features/Domains/DomainUpsertModel.cs
+++ PABC.Server/Features/Domains/DomainUpsertModel.cs
@@ -1,13 +1,17 @@
-﻿using PABC.Data;
+﻿using Json.Schema.Generation;
+using System.ComponentModel.DataAnnotations;
+using PABC.Data;
 
 namespace PABC.Server.Features.Domains
 {
     public class DomainUpsertModel
     {
         [Json.Schema.Generation.MaxLength(PabcDbContext.MaxLengthForIndexProperties)]
+        [System.ComponentModel.DataAnnotations.MaxLength(PabcDbContext.MaxLengthForIndexProperties)]
         public required string Name { get; init; }
 
         [Json.Schema.Generation.MaxLength(PabcDbContext.MaxLengthForIndexProperties)]
+        [System.ComponentModel.DataAnnotations.MaxLength(PabcDbContext.MaxLengthForIndexProperties)]
         public required string Description { get; init; }
     }
 }
