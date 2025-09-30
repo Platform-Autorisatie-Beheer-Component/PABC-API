413ffa31650777db4572777ea7e3a41ff57104dd
nijmra <armijn@gmail.com>
2025-09-30 16:24:31 +0200

    setup endpoints

---
+++ PABC.Server/Features/EntityTypes/PostEntityType/PostEntityTypeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PABC.Data;
+using PABC.Data.Entities;
+
+namespace PABC.Server.Features.EntityTypes.PostEntityType
+{
+    [ApiController]
+    [Route("/api/v1/entity-types")]
+    public class PostEntityTypeController(PabcDbContext db) : Controller
+    {
+        [HttpPost]
+        [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status409Conflict, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType<EntityType>(StatusCodes.Status201Created, MediaTypeNames.Application.Json)]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError, MediaTypeNames.Application.ProblemJson)]
+        public async Task<IActionResult> PostEntityType([FromBody] EntityTypeUpsertModel model, CancellationToken token = default)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            try
+            {
+                var entityType = new EntityType
+                {
+                    Id = Guid.NewGuid(),
+                    EntityTypeId = model.EntityTypeId,
+                    Type = model.Type,
+                    Name = model.Name
+                };
+                
+                db.EntityTypes.Add(entityType);
+                
+                await db.SaveChangesAsync(token);
+
+                return StatusCode(201, entityType);
+            }
+            catch (DbUpdateException ex) when (ex.IsDuplicateException())
+            {
+                return Conflict(new ProblemDetails
+                {
+                    Title = "Duplicate EntityTypeId / Type combination",
+                    Status = StatusCodes.Status409Conflict
+                });
+            }
+            catch
+            {
+                return StatusCode(500, new ProblemDetails
+                {
+                    Title = "Internal Server Error",
+                    Status = StatusCodes.Status500InternalServerError
+                });
+            }
+        }
+    }
+}
---
+++ PABC.Server/Features/EntityTypes/PutEntityType/PutEntityTypeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PABC.Data;
+using PABC.Data.Entities;
+
+namespace PABC.Server.Features.EntityTypes.PutEntityType
+{
+    [ApiController]
+    [Route("/api/v1/entity-types")]
+    public class PutEntityTypeController(PabcDbContext db) : Controller
+    {
+        [HttpPut("{id}")]
+        [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status409Conflict, MediaTypeNames.Application.ProblemJson)]
+        [ProducesResponseType<EntityType>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)]
+        [ProducesResponseType<ProblemDetails>(StatusCodes.Status500InternalServerError, MediaTypeNames.Application.ProblemJson)]
+        public async Task<IActionResult> PutEntityType(Guid id, [FromBody] EntityTypeUpsertModel model, CancellationToken token = default)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            try
+            {
+                var entityType = await db.EntityTypes.FindAsync([id], token);
+
+                if (entityType == null)
+                {
+                    return NotFound(new ProblemDetails
+                    {
+                        Title = "Entity Type Not Found",
+                        Status = StatusCodes.Status404NotFound
+                    });
+                }
+
+                entityType.EntityTypeId = model.EntityTypeId;
+                entityType.Type = model.Type;
+                entityType.Name = model.Name;
+
+                db.EntityTypes.Update(entityType);
+                
+                await db.SaveChangesAsync(token);
+
+                return Ok(entityType);
+            }
+            catch (DbUpdateException ex) when (ex.IsDuplicateException())
+            {
+                return Conflict(new ProblemDetails
+                {
+                    Title = "Duplicate EntityTypeId / Type combination",
+                    Status = StatusCodes.Status409Conflict
+                });
+            }
+            catch
+            {
+                return StatusCode(500, new ProblemDetails
+                {
+                    Title = "Internal Server Error",
+                    Status = StatusCodes.Status500InternalServerError
+                });
+            }
+        }
+    }
+}
