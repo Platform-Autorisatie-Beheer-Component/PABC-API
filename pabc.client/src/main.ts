dcc7b24d09c598f66f8161808ab6cfae79c05454
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-10-27 14:16:48 +0100

    cleanup

--- pabc.client/src/main.ts
+++ pabc.client/src/main.ts
@@ -3,15 +3,10 @@ import "./assets/main.scss";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import { pinia } from "./stores";
 
 const app = createApp(App);
 
-
-app.use(pinia);
-
 (async () => {
-
   // Load router after theme, to be able to use theme settings
   const { default: router } = await import("./router");
   const { default: routerGuardsPlugin } = await import("./plugins/routerGuards");
