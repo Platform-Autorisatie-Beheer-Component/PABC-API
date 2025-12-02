daae302e516bac492369810d5fa1f7dc06a404bd
Felix Cornelissen <felix@icatt.nl>
2025-12-02 11:36:16 +0100

    wip

--- PABC.AppHost/Program.cs
+++ PABC.AppHost/Program.cs
@@ -1,4 +1,4 @@
-﻿using Aspire.Hosting;
+﻿using Npgsql;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -9,7 +9,8 @@ var postgres = builder.AddPostgres("postgres")
 
 var postgresdb = postgres.AddDatabase("Pabc");
 
-var keycloak = builder.AddKeycloak("keycloak", 8080, adminPassword: builder.AddParameter("keycloakAdminPassword", "unsafe-development-password", secret: true))
+
+var keycloak = builder.AddKeycloak("keycloak", port: 61046, adminPassword: builder.AddParameter("keycloakAdminPassword", "unsafe-test-password", secret: true))
     .WithRealmImport("./Realms")
     .WithLifetime(ContainerLifetime.Persistent);
 
@@ -20,12 +21,13 @@ var migrations = builder.AddProject<Projects.PABC_MigrationService>("migrations"
 
 builder.AddProject<Projects.PABC_Server>("pabc-server")
     .WithEnvironment("API_KEY__0", "unsafe-test-api-key")
-    .WithEnvironment("Oidc__Authority", "http://localhost:8080/realms/pabc/")
+    .WithEnvironment("Oidc__Authority", "http://localhost:61046/realms/pabc/")
     .WithEnvironment("Oidc__ClientId", "pabc")
-    .WithEnvironment("Oidc__ClientSecret", "unsafe-development-secret")
+    .WithEnvironment("Oidc__ClientSecret", "unsafe-test-secret")
     .WithEnvironment("Oidc__RequireHttps", "false")
+    .WithEnvironment("Oidc__FunctioneelBeheerderRole", "administrator")
     .WithEnvironment("KeycloakAdmin__ClientId", "pabc-admin-client")
-    .WithEnvironment("KeycloakAdmin__ClientSecret", "unsafe-development-secret")
+    .WithEnvironment("KeycloakAdmin__ClientSecret", "unsafe-test-secret")
     .WithReference(postgresdb)
     .WithReference(keycloak)
     .WaitFor(keycloak)
